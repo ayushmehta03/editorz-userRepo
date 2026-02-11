4827e99f0c12924a0487160fd11341ad907a8dd7
Ayush Mehta <ayushmehtabps@gmail.com>
2026-02-11 23:00:09 +0530

    add resend in phone numbers

--- backend/internal/controllers/resend_controller.go
+++ backend/internal/controllers/resend_controller.go
@@ -1,9 +1,10 @@
-package services
+package controllers
 
 import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/ayushmehta03/editorz-userRepo/backend/internal/database"
@@ -12,6 +13,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -90,3 +92,85 @@ func ResendEmailOtp(client *mongo.Client, redisClient *redis.Client)gin.HandlerF
 
 	}
 }
+
+ // resend phone otp with cooldown
+
+ func ResendPhoneOtp(client *mongo.Client,redisClient *redis.Client)gin.HandlerFunc{
+	return func(c *gin.Context){
+
+		var req struct{
+			UserID string `json:"user_id" binding:"required"`
+		}
+
+
+		if err:=c.ShouldBindJSON(&req);err!=nil{
+			c.JSON(http.StatusBadRequest,gin.H{"error":"User Id required"})
+			return 
+		}
+
+		userID,err:=primitive.ObjectIDFromHex(req.UserID)
+
+		if err!=nil{
+			c.JSON(http.StatusBadRequest,gin.H{"error":"Invalid user id"})
+			return 
+		}
+
+		ctx,cancel:=context.WithTimeout(context.Background(),10*time.Second)
+
+		defer cancel()
+
+		editorCollection:=database.OpenCollection("editors",client)
+
+		var user models.User
+
+		if err:=editorCollection.FindOne(ctx,bson.M{"_id":userID}).Decode(&user);err!=nil{
+			c.JSON(http.StatusNotFound,gin.H{"error":"User not found"})
+			return 
+		}
+		if user.IsPhoneVerified {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Phone already verified"})
+			return
+		}
+
+		countryCode := os.Getenv("MESSAGE_CENTRAL_COUNTRY_CODE")
+
+		// Rate limit
+		err = utils.HandleOtpResendBackoff(
+			redisClient,
+			fmt.Sprintf("otp:phone:%s:%s", countryCode, user.Phone),
+		)
+		if err != nil {
+			c.JSON(http.StatusTooManyRequests, gin.H{"error": err.Error()})
+			return
+		}
+
+		verificationID, err := utils.MessageCentralSendOTP(user.Phone)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send OTP"})
+			return
+		}
+
+		_, err = editorCollection.UpdateOne(
+			ctx,
+			bson.M{"_id": user.ID},
+			bson.M{
+				"$set": bson.M{
+					"verification_id": verificationID,
+					"updated_at":      time.Now(),
+				},
+			},
+		)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update verification"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Phone OTP resent successfully",
+		})
+
+
+
+
+	}
+ }
